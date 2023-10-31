3191a05e0cad1bb1b2bc20ec667b4fea109de95d
Shujian Qian <shujian.qian@outlook.com>
2023-10-31 16:33:47 -0400

    refactor cmakelist

--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 //
-// Created by Shujian Qian on 2023-07-18.
+// Created by Shujian Qian on 2023-08-08.
 //
 
-#include "epic_gpu.cuh"
+#include <iostream>
 
+#include "gpu_execution_planner.h"
+#include "gpu_allocator.h"
+#include "benchmarks/ycsb.h"
 #include "util_log.h"
-#include "util_math.h"
-
 #include "txn_bridge.h"
+
+#include "benchmarks/tpcc.h"
 
 int main(int argc, char **argv)
 {
 
-#ifdef EPIC_CUDA_AVAILABLE
-    epic_gpu(argc, argv);
+    epic::tpcc::TpccConfig config;
+    config.epochs = 5;
+    config.num_warehouses = 1;
+    //    config.num_txns = 10;
 
-    run_thrust_experiment(argc, argv);
-#endif
-
-    epic::TxnBridge bridge();
-
-    epic::Logger &logger = epic::Logger::GetInstance();
-    logger.Trace("help {}", epic::formatSizeBytes(1024 * 1024 * 1024));
+    epic::tpcc::TpccDb db(config);
+    db.loadInitialData();
+    db.generateTxns();
+    db.runBenchmark();
 
     return 0;
 }
--- tests/unit_tests/test_gpu_table.cu
+++ tests/unit_tests/test_gpu_table.cu
@@ -5,35 +5,25 @@
 #include <iostream>
 
 #include "gpu_execution_planner.h"
-#include "benchmarks/gpu_ycsb.cuh"
 #include "gpu_allocator.h"
 #include "benchmarks/ycsb.h"
 #include "util_log.h"
 #include "txn_bridge.h"
-#include "gpu_txn.cuh"
 
 #include "benchmarks/tpcc.h"
 
 int main(int argc, char **argv)
 {
-//    epic::GpuAllocator allocator;
-//    epic::GpuTableExecutionPlanner table("table1", allocator, 1000, 10, 100'000, 1'000'000);
-//    table.Initialize();
-//    gpu_err_check(cudaSetDeviceFlags(cudaDeviceScheduleYield));
 
     epic::tpcc::TpccConfig config;
     config.epochs = 5;
     config.num_warehouses = 1;
-//    config.num_txns = 10;
+    //    config.num_txns = 10;
+
     epic::tpcc::TpccDb db(config);
-//    auto db2 = new epic::tpcc::TpccDb(config);
-//    db2->loadInitialData();
-//    db2->generateTxns();
-//    db2->runBenchmark();
     db.loadInitialData();
     db.generateTxns();
     db.runBenchmark();
-
 
     return 0;
 }
